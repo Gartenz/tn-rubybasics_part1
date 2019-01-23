43f7a866b988f8cd9ce09dd0759d5b004cd0641c
Alexey Frolov <gartenzius@gmail.com>
2019-01-23 22:24:51 +0300

    Fix new remarks

--- pifagor.rb
+++ pifagor.rb
@@ -1,5 +1,5 @@
 def is_rectangular?(a,b,c)
-	return (a**2 + b**2) == c**2
+  return (a**2 + b**2) == c**2
 end
 
 puts "Введите длину первой стороны треугольника"
@@ -12,22 +12,22 @@ triangle_side3 = gets.chomp.to_f
 triangle_def = ""
 
 if triangle_side1 == triangle_side2 && triangle_side2 == triangle_side3
-	triangle_def = "Равносторонний треугольник"
+  triangle_def = "Равносторонний треугольник"
 else
-	#Записываем в массив для поиска минимальныйх и максимальных значений
-	triangle_sides = [triangle_side1,triangle_side2,triangle_side3]
-	#Достаем 2 минимальных элемента
-	c1,c2 = triangle_sides.min(2)
-	h = triangle_sides.max
-	if c1 == c2 
-		triangle_def += "Равнобедренный" 
-	else
-		triangle_def += "Обычный"
-	end
-	if is_rectangular?(c1,c2,h) 
-		triangle_def += " прямоугольный треугольник"
-	else 
-		triangle_def += " не прямоугольный треугольник"
-	end
+  #Записываем в массив для поиска минимальныйх и максимальных значений
+  triangle_sides = [triangle_side1,triangle_side2,triangle_side3]
+  #Достаем 2 минимальных элемента
+  c1, c2 = triangle_sides.min(2)
+  h = triangle_sides.max
+  if c1 == c2 
+    triangle_def += "Равнобедренный" 
+  else
+    triangle_def += "Обычный"
+  end
+  if is_rectangular?(c1,c2,h) 
+    triangle_def += " прямоугольный треугольник"
+  else 
+    triangle_def += " не прямоугольный треугольник"
+  end
 end
 puts triangle_def
--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -8,11 +8,11 @@ c = gets.chomp.to_f
 puts "Ищем дискриминант..."
 d = b**2 - 4 * a * c
 if d >= 0 
-	puts "Корни вещественные"
-	sqrt = Math.sqrt(d)
-	x1 = (-b - sqrt) / (2 * a)
-	x2 = (-b + sqrt) / (2 * a)
-	puts "Корни уравнения #{x1} и #{x2}"
+  puts "Корни вещественные"
+  sqrt = Math.sqrt(d)
+  x1 = (-b - sqrt) / (2 * a)
+  x2 = (-b + sqrt) / (2 * a)
+  puts "Корни уравнения #{x1} и #{x2}"
 else
-	puts "Корни мнимые"
+  puts "Корни мнимые"
 end
