1388242068470cc8cf7fbfc5e35be5b3bf814122
Alexey Frolov <gartenzius@gmail.com>
2019-01-23 14:18:06 +0300

    fixed remarks2

--- perfect_weight.rb
+++ perfect_weight.rb
@@ -1,6 +1,6 @@
 puts "Как Вас зовут?"
 user_name = gets.chomp
 puts "Какой у Вас рост?"
-user_height = gets.chomp
-weight = user_height.to_i - 110
+user_height = gets.chomp.to_i
+weight = user_height - 110
 puts weight <= 0 ? "Ваш вес идеален #{user_name}!" : "#{user_name}, Ваш вес #{weight}."
